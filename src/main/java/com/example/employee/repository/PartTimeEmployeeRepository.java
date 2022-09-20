e06dff07e0d726ab44a5d654b347b850ae2c3206
felixmaina <karani.maina2010@gmail.com>
2022-09-20 23:40:32 +0300

    set the repository class

--- src/main/java/com/example/employee/repository/PartTimeEmployeeRepository.java
+++ src/main/java/com/example/employee/repository/PartTimeEmployeeRepository.java
@@ -2,9 +2,10 @@ package com.example.employee.repository;
 
 import com.example.employee.models.PartTimeEmployee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface PartTimeEmployeeRepository extends JpaRepository<PartTimeEmployee,Long> {
 
 

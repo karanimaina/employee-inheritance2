286a82dab5ee331570bda4c7f6e57e903cf365cb
felixmaina <karani.maina2010@gmail.com>
2022-09-20 23:27:35 +0300

    add  a primary id attribbute

--- src/main/java/com/example/employee/models/Employee.java
+++ src/main/java/com/example/employee/models/Employee.java
@@ -12,7 +12,6 @@ import javax.persistence.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-
 //single table inheritance
 //@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @MappedSuperclass  //mapped superclass cannot be an entity

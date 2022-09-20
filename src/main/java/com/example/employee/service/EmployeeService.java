8ab23293e4e9ed6f1412f1bfdcaa3386c4703d29
felixmaina <karani.maina2010@gmail.com>
2022-09-20 23:47:23 +0300

    autowire the fullrtime repository

--- src/main/java/com/example/employee/service/EmployeeService.java
+++ src/main/java/com/example/employee/service/EmployeeService.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EmployeeService{
 private final FullTimeRepository fullTimeRepository;
+
 private final PartTimeEmployeeRepository partTimeEmployeeRepository;
 
 

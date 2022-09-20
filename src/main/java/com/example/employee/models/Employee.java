d0e482b031eace93842d0771dfcb0a709bd0fbd0
felixmaina <karani.maina2010@gmail.com>
2022-09-20 23:57:55 +0300

    disable gtable class inheritance

--- src/main/java/com/example/employee/models/Employee.java
+++ src/main/java/com/example/employee/models/Employee.java
@@ -16,6 +16,7 @@ import javax.persistence.*;
 //@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @MappedSuperclass  //mapped superclass cannot be an entity
 public abstract  class Employee {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;

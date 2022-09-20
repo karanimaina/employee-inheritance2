d6540fe223f53228ae2d73810048911bd0a00d13
felixmaina <karani.maina2010@gmail.com>
2022-09-20 23:34:03 +0300

    set the Getter anotation

--- src/main/java/com/example/employee/models/FullTimeEmployee.java
+++ src/main/java/com/example/employee/models/FullTimeEmployee.java
@@ -8,11 +8,11 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 
 @Entity
-
 @Getter
 @NoArgsConstructor
 public class FullTimeEmployee extends Employee {
     private long salary;
+
 
     public FullTimeEmployee(long id, String name, long salary) {
         super(id, name);
